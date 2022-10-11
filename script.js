2e7e2717569219001e207b0045c8bcc69530def3
web-developer001 <lochinquvondiqov31@gmail.com>
2022-10-11 15:19:02 +0500

    JC-lesson-1

---
+++ script.js
@@ -0,0 +1,68 @@
+
+
+
+// 1-masala;
+
+// let L = prompt ("RAQAM KIRITING santimetrda");
+// let M =L/100;
+// console.log(M +" metr") ;
+ 
+
+// 2-masala;
+
+// let L = prompt ("RAQAM KIRITING kilogramda");
+// let M =L/1000;
+// console.log(M +" tonna") ;
+ 
+
+// 3-masala;
+
+// let A = prompt ("Birinchi sonni kiriting");
+// let B = prompt ("Ikkinchi sonni kiriting");
+// let x = A/B;
+// console.log(x+ " marta joylashtirish mumkin")
+
+
+// 4-masala;
+
+// let xy=prompt("Ikki xonali son kiriting");
+// let x=Math.floor(xy/10);
+// let y=xy%10;
+// console.log(x+ " ta o'nliklar")
+// console.log(y+ " ta birliklar")
+
+ 
+// 5-masala;
+
+// let x=2;
+// let y=4;
+// let z=x+y;
+// console.log(z+" ikki honali sonning raqamlari yig'indisi")
+
+
+// 6-masala;
+
+// let x=2;
+// let y=4;
+// let z=x*y;
+// console.log(z+" ikki honali sonning raqamlari ko'paytmasi")
+
+
+// 7-masala;
+
+// let A=prompt("999 dan katta son kiriting");
+// let x=Math.floor(A/100);
+// let y=A%100;
+// console.log(x+" ta yuzliklar")
+// console.log(y+' qoldiq')
+
+
+// 8-masala;
+
+// let A=prompt("Sekundni kiriting");
+// let x=A/60;
+// let y=A/3600;
+// console.log(x+" minut o'tdi")
+// console.log(y+" soat o'tdi")
+
+
